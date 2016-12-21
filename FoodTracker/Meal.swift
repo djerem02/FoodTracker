92118a7f80e97ca130530bc72128bdd28294cf43
Jérémy <jeremy@MacBook-Pro-de-Jeremy.local>
2016-12-21 17:24:30 +0100

    Building the UI

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,38 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jérémy on 21/12/2016.
+//  Copyright © 2016 Jérémy. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+    
+}
